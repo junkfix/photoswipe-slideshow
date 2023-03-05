8388b1a220e72891eab56141673a26c26c55755b
dpet23 <dpet23@users.noreply.github.com>
2023-03-05 22:25:30 +1100

    Added a progress bar to show the time before going to the next slide

--- photoswipe-slideshow.esm.js
+++ photoswipe-slideshow.esm.js
@@ -10,11 +10,12 @@ const defaultOptions = {
     delayMs: 1000,
     buttonTitle: 'Toggle slideshow',
     buttonOrder: 6,  // default: counter=5, image=7, zoom=10, info=15, close=20
+    showProgressBar: true,
 };
 
 export default class PhotoSwipeSlideshow {
     slideshow_is_running = false;
-    buttonSVG =
+    slideshowButtonSVG =
         '<svg class="pswp__icn" viewBox="0 0 32 42.667">' +
         // Icon outlines
         '<use class="pswp__icn-shadow" xlink:href="#pswp__slideshow-icn-play"/>' +
@@ -24,6 +25,11 @@ export default class PhotoSwipeSlideshow {
         // Stop icon
         '<path id="pswp__slideshow-icn-stop" transform="translate(6,6)" style="scale:0.8; display:none;" d="M0 10.667C0 7.725 2.392 5.333 5.333 5.333H26.667c2.942 0 5.333 2.392 5.333 5.333V32c0 2.942 -2.392 5.333 -5.333 5.333H5.333c-2.942 0 -5.333 -2.392 -5.333 -5.333V10.667z" />' +
         '</svg>';
+    slideshowProgressBarElement = null;
+    slideshowProgressBarWidth = 0;
+    slideshowProgressBarWidthMax = 100;
+    slideshowProgressBarUpdateIntervalMs = 100;
+    slideshowProgressBarIntervalID = 0;
 
     /**
      * Set up PhotoSwipe lightbox event binds.
@@ -51,18 +57,19 @@ export default class PhotoSwipeSlideshow {
      * Set up PhotoSwipe lightbox event binds.
      */
     initLightboxEvents = () => {
-        const { lightbox, gotoNextSlide, getTimeout } = this;
+        const { lightbox, goToNextSlideAfterTimeout } = this;
 
         // When slide is switched by the slideshow, start a timer for switching to the next slide.
         lightbox.on('change', () => {
             if (this.slideshow_is_running) {
-                setTimeout(gotoNextSlide, getTimeout());
+                goToNextSlideAfterTimeout();
             }
         });
 
         // Close the slideshow when closing PhotoSwipe.
         lightbox.on('close', () => {
             this.slideshow_is_running = false;
+            this.resetProgressBar();
         });
     }
 
@@ -70,36 +77,50 @@ export default class PhotoSwipeSlideshow {
      * Set up PhotoSwipe gallery event binds.
      */
     initGalleryEvents = () => {
-        const { pswp, options, buttonSVG, setSlideshowState } = this;
+        const { pswp, options, slideshowButtonSVG, setSlideshowState } = this;
 
-        // Add a button to the PhotoSwipe UI.
+        // Add UI elements to an open gallery.
         pswp.on('uiRegister', () => {
+            // Add a button to the PhotoSwipe UI for toggling the slideshow state.
             pswp.ui.registerElement({
-                name: 'slideshow',
+                name: 'slideshow',  // button.pswp__button--slideshow
                 title: options.buttonTitle,
                 order: options.buttonOrder,
                 isButton: true,
-                html: buttonSVG,
+                html: slideshowButtonSVG,
                 onClick: (event, buttonElement) => {
                     setSlideshowState();
                 }
             });
+
+            // Add a slideshow progress bar to the PhotoSwipe UI.
+            if (options.showProgressBar) {
+                pswp.ui.registerElement({
+                    name: 'slideshow-progress-bar',  // div.pswp__slideshow-progress-bar
+                    appendTo: 'wrapper',
+                    onInit: (progressBarElement, pswpInstance) => {
+                        this.slideshowProgressBarElement = progressBarElement;
+                        this.setProgressBarWidth(0);
+                    },
+                });
+            }
         });
-    }
+    };
 
     /**
      * Toggle the slideshow state and switch the button's icon.
      */
     setSlideshowState = () => {
-        const { gotoNextSlide, getTimeout } = this;
-
         // Update the slideshow running state.
         this.slideshow_is_running = !this.slideshow_is_running;
 
-        // Go to next slide after some wait time.
-        // The `afterChange` listener triggers further timers.
         if (this.slideshow_is_running) {
-            setTimeout(gotoNextSlide, getTimeout());
+            // Starting the slideshow: go to next slide after some wait time.
+            // The `change` listener triggers further timers.
+            this.goToNextSlideAfterTimeout();
+        } else {
+            // Stopping the slideshow: reset the progress bar.
+            this.resetProgressBar();
         }
 
         // Update icon to show the slideshow running state.
@@ -114,12 +135,18 @@ export default class PhotoSwipeSlideshow {
     /**
      * Go to the next slide, if the slideshow is currently running.
      */
-    gotoNextSlide = () => {
-        const { slideshow_is_running, pswp } = this;
+    goToNextSlideAfterTimeout = () => {
+        const { getTimeout, animateProgressBar } = this;
 
-        if (slideshow_is_running) {
-            pswp.next();
-        }
+        const nextSlideTimeout = getTimeout();
+
+        setTimeout(() => {
+            if (this.slideshow_is_running) {
+                pswp.next();
+            }
+        }, nextSlideTimeout);
+
+        animateProgressBar(nextSlideTimeout);
     }
 
     /**
@@ -150,5 +177,63 @@ export default class PhotoSwipeSlideshow {
 
         // Use the default delay for images.
         return options.delayMs;
+    }
+
+    /**
+     * Progressively fill the slideshow progress bar as the slide timer progresses.
+     *
+     * @param {number} nextSlideTimeout The time before going to the next slide, in milliseconds.
+     */
+    animateProgressBar = (nextSlideTimeout) => {
+        const { options, resetProgressBar, slideshowProgressBarWidthMax, slideshowProgressBarUpdateIntervalMs, stopProgressbar, setProgressBarWidth } = this;
+
+        if (options.showProgressBar) {
+            // Ensure the progress bar starts in a clean state for this slide.
+            resetProgressBar();
+
+            // Calculate the amount to progress the bar in each step.
+            // Use (timeout - an interval) as the total, to ensure we see the bar at 100%.
+            const progressBarIncreaseAmount =
+                slideshowProgressBarWidthMax /
+                ((nextSlideTimeout - slideshowProgressBarUpdateIntervalMs) / slideshowProgressBarUpdateIntervalMs);
+
+            // Start a timer to slowly fill in the progress bar.
+            this.slideshowProgressBarIntervalID = setInterval(() => {
+                if (this.slideshowProgressBarWidth >= slideshowProgressBarWidthMax) {
+                    // Bar has been filled, stop the animation.
+                    stopProgressbar();
+                } else {
+                    // Still need to wait for the next slide, fill in a bit of the progress bar.
+                    setProgressBarWidth(this.slideshowProgressBarWidth + progressBarIncreaseAmount);
+                }
+            }, slideshowProgressBarUpdateIntervalMs);
+        }
+    }
+
+    /**
+     * Set the width of the slideshow progress bar.
+     *
+     * @param {number} width The new width, as a percentage.
+     */
+    setProgressBarWidth = (width) => {
+        this.slideshowProgressBarWidth = width;
+        this.slideshowProgressBarElement.style.width = `${width}%`;
+    }
+
+    /**
+     * Stop animating the progress bar by clearing the timed function calls.
+     */
+    stopProgressbar = () => {
+        clearInterval(this.slideshowProgressBarIntervalID);
+    }
+
+    /**
+     * Fully reset the progress bar: stop the animation and reset the width.
+     */
+    resetProgressBar = () => {
+        if (this.options.showProgressBar) {
+            this.stopProgressbar();
+            this.setProgressBarWidth(0);
+        }
     }
 }
