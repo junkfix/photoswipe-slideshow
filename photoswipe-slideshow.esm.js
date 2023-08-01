293253f5d11512f06b189db4cf64de11c41432bc
dpet23 <dpet23@users.noreply.github.com>
2023-08-01 21:57:19 +1000

    Cleaned up existing code
    * Added comments explaining the code
    * Renamed variables and functions
    
    The code in the ".esm.js" file can be verbose, and doesn't need obfuscation. The minification process will still provide a sufficiently small file.

--- photoswipe-slideshow.esm.js
+++ photoswipe-slideshow.esm.js
@@ -1,55 +1,80 @@
-/*! PhotoSwipe slideshow plugin v1.7  https://github.com/htmltiger/photoswipe-slideshow */
+/*! Slideshow plugin for PhotoSwipe v5.  https://github.com/dpet23/photoswipe-slideshow */
 
 class PhotoSwipeSlideshow {
+
+    /**
+     * Set up PhotoSwipe lightbox event binds.
+     *
+     * @param {PhotoSwipeLightbox} lightbox PhotoSwipe lightbox instance.
+     * @param {Number} time                 Slideshow delay in seconds.
+     */
     constructor(lightbox, time) {
+        // Add custom CSS for the progress bar.
         document.head.insertAdjacentHTML(
             'beforeend',
             '<style>.pswp__timer{height:3px;background:#c00;width:0;position:fixed;bottom:0}.load .pswp__timer{width:100%}</style>',
         );
-        this.swake = false;
-        this.wakeo = null;
-        this.state = -1;
-        this.timert = 0;
+
+        // Set default parameters.
+        this.wakeLockState = false;
+        this.wakeLockSentinel = null;
+        this.state = -1; // <0 (undefined) // ==0 (paused) // >0 (running)
+        this.slideshowTimerID = 0;
         this.time = Number(localStorage.getItem('pswptime'));
         this.time = this.time > 0 ? this.time : time ? time : 4;
         this.lightbox = lightbox;
+
+        // Set up lightbox and gallery event binds.
         this.lightbox.on('init', () => {
             this.init(this.lightbox.pswp);
         });
     }
 
+    /**
+     * Set up event binds for the PhotoSwipe lightbox and gallery.
+     *
+     * @param {PhotoSwipeCore} pswp PhotoSwipe instance.
+     */
     init(pswp) {
+
+        // Add UI elements to an open gallery.
         pswp.on('uiRegister', () => {
+            // Add a button to the PhotoSwipe UI for toggling the slideshow state.
             pswp.ui.registerElement({
-                name: 'playpause-button',
+                name: 'playpause-button', // pswp__button--playpause-button
                 title: 'Play [Space] Time +/-',
-                order: 18,
+                order: 18, // default: counter=5, image=7, zoom=10, info=15, close=20
                 isButton: true,
                 html: '<svg aria-hidden="true" class="pswp__icn" viewBox="0 0 32 32"><use class="pswp__icn-shadow" xlink:href="#pswp__icn-pause"/><use class="pswp__icn-shadow" xlink:href="#pswp__icn-play"/><path id="pswp__icn-play" d="M7.4 25 25 16 7.4 6.6Z" /><path id="pswp__icn-pause" style="display:none" d="m7 7h4l0 18h-4zm14 0h4v18h-4z"/></svg>',
                 onClick: (event, el) => {
                     this.player();
                 },
             });
+
+            // Add a wrapper for the progress bar.
             pswp.ui.registerElement({
                 name: 'playtime',
                 className: 'pswp__time',
                 appendTo: 'wrapper',
             });
+
+            // Add custom keyboard bindings.
             pswp.events.add(document, 'keydown', e => {
                 const k = e.keyCode;
-                if (k == 32) {
+                if (k == 32) {  // Space
                     this.player();
                     e.preventDefault();
                 }
-                if (k == 38 || k == 107 || k == 187) {
+                if (k == 38 || k == 107 || k == 187) {  // ArrowUp || NumpadAdd || Equal
                     this.timer(1);
                 }
-                if (k == 40 || k == 109 || k == 189) {
+                if (k == 40 || k == 109 || k == 189) {  // ArrowDown || NumpadSubtract || Minus
                     this.timer(-1);
                 }
             });
         });
 
+        // Close the slideshow when closing PhotoSwipe.
         this.lightbox.on('close', () => {
             if (this.state > 0) {
                 this.player();
@@ -57,99 +82,150 @@ class PhotoSwipeSlideshow {
         });
     }
 
+    /**
+     * Define the slideshow state and start/stop the slideshow.
+     */
     player() {
         if (this.state < 0) {
-            var t = this;
-            setTimeout(function () {
-                t.stimer();
+            setTimeout(() => {
+                this.setSlideshowState();
             }, 600);
-            t.state = 0;
+            this.state = 0;
         } else {
-            this.stimer();
+            this.setSlideshowState();
         }
     }
 
-    stimer(h) {
+    /**
+     * Toggle the slideshow state and switch the button's icon.
+     */
+    setSlideshowState() {
+        // Invert the slideshow state.
         this.state = this.state ? 0 : 1;
-        if (!this.state) {
-            this.bar(0);
-            clearTimeout(this.timert);
-            this.timert = null;
-        } else {
+
+        if (this.state) {
+            // Starting the slideshow: go to next slide after some wait time.
             this.timer();
+        } else {
+            // Stopping the slideshow: reset the progress bar and timer.
+            this.toggleProgressBar(0);
+            clearTimeout(this.slideshowTimerID);
+            this.slideshowTimerID = null;
         }
+
+        // Update icon to reflect the slideshow state.
         document.querySelector('#pswp__icn-pause').style.display = this.state ? 'inline' : 'none';
         document.querySelector('#pswp__icn-play').style.display = this.state ? 'none' : 'inline';
-        this.wake(this.state);
+
+        // Toggle wake lock: prevent/allow the screen to turn off.
+        this.toggleWakeLock(this.state);
     }
 
-    timer(k, c) {
-        this.bar(0);
-        if (this.timert) {
-            clearTimeout(this.timert);
-            this.timert = null;
+    /**
+     * Manage the slideshow timer.
+     *
+     * @param {Number | undefined} lengthDelta Amount to change the slideshow length.
+     */
+    timer(lengthDelta) {
+        // Reset the current timer progress.
+        this.toggleProgressBar(0);
+        if (this.slideshowTimerID) {
+            clearTimeout(this.slideshowTimerID);
+            this.slideshowTimerID = null;
         }
+
+        // Don't restart the timer if the slideshow isn't running.
         if (this.state < 1) {
             return;
         }
-        if (k) {
-            this.time = Math.max(1, this.time + k);
+
+        // Change the slideshow length.
+        if (lengthDelta) {
+            // Save the updated slideshow length.
+            this.time = Math.max(1, this.time + lengthDelta);
             localStorage.setItem('pswptime', this.time);
-            c = document.querySelector('.pswp__counter');
-            if (c) {
-                c.innerHTML = this.time + 's';
+
+            // Show the current slideshow length.
+            const slideCounterElement = document.querySelector('.pswp__counter');
+            if (slideCounterElement) {
+                slideCounterElement.innerHTML = this.time + 's';
             }
         }
-        if (!pswp.currSlide.content.isLoading()) {
-            this.timert = setTimeout(this.timer.bind(this), this.time * 1000);
-            if (!k) {
+
+        if (pswp.currSlide.content.isLoading()) {
+            this.slideshowTimerID = setTimeout(() => {this.timer()}, 200);
+        } else {
+            // Start the slideshow timer and go to the next slide.
+            this.slideshowTimerID = setTimeout(() => {this.timer()}, this.time * 1000);
+            if (!lengthDelta) {
                 pswp.next();
             }
-            this.bar(2);
-        } else {
-            this.timert = setTimeout(this.timer.bind(this), 200);
+            this.toggleProgressBar(2);
         }
     }
 
-    bar(a, b) {
-        b = document.querySelector('.pswp__time');
-        if (a) {
-            if (a == 2) {
-                b.innerHTML = '<div class="pswp__timer" style="transition:width ' + this.time + 's;"></div>';
-                setTimeout(this.bar.bind(this), 100, 1);
+    /**
+     * Show or hide the slideshow progress bar.
+     *
+     * @param {Number | undefined} code Functionality: 2==start, 1==running, 0==stop.
+     */
+    toggleProgressBar(code) {
+        const slideshowProgressBarWrapper = document.querySelector('.pswp__time');
+        if (code) {
+            if (code == 2) {
+                slideshowProgressBarWrapper.innerHTML = `<div class="pswp__timer" style="transition:width ${this.time}s;"/>`;
+                setTimeout(() => {this.toggleProgressBar(1)}, 100);
                 return;
             }
-            b.classList.add('load');
+            slideshowProgressBarWrapper.classList.add('load');
         } else {
-            b.innerHTML = '';
-            b.classList.remove('load');
+            slideshowProgressBarWrapper.innerHTML = '';
+            slideshowProgressBarWrapper.classList.remove('load');
         }
     }
 
-    wake(n) {
-        if (this.swake == n) {
+    /**
+     * Set wake lock if supported by the browser.
+     * https://caniuse.com/wake-lock
+     *
+     * @param {Number} state Requested wake lock state: 1==on, 0==off
+     */
+    toggleWakeLock(state) {
+        if (this.wakeLockState == state) {
             return;
         }
+
         if ('keepAwake' in screen) {
-            screen.keepAwake = n;
+            // Use experimental API for older browsers.
+            // This is a simple boolean flag.
+            screen.keepAwake = state;
         } else if ('wakeLock' in navigator) {
-            if (this.wakeo) {
-                this.wakeo.release();
-                this.wakeo = null;
+            // Use the Screen Wake Lock API for newer browsers.
+
+            if (this.wakeLockSentinel) {
+                // Release screen wake lock, if a request was previously successful.
+                this.wakeLockSentinel.release().then(() => {
+                    this.wakeLockSentinel = null;
+                });
             } else {
+                // Request screen wake lock.
                 navigator.wakeLock
                     .request('screen')
-                    .then(w => {
-                        this.wakeo = w;
-                        this.wakeo.addEventListener('release', () => {
-                            this.wakeo = null;
-                            this.swake = false;
+                    .then(sentinel => {
+                        // Save the reference for the wake lock.
+                        this.wakeLockSentinel = sentinel;
+
+                        // Update our state if the wake lock happens to be released by the browser.
+                        this.wakeLockSentinel.addEventListener('release', () => {
+                            this.wakeLockSentinel = null;
+                            this.wakeLockState = false;
                         });
                     })
-                    .catch(e => {});
+                    .catch(e => {}); // ignore errors if wake lock request fails.
             }
         }
-        this.swake = n;
+
+        this.wakeLockState = state;
     }
 }
 
